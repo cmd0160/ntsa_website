d7b8265db09ccb20bdbdf2b5e9741250c7a980a0
cdtest <corydavis1989@gmail.com>
2022-06-07 12:01:50 -0500

    added auth.js for user authentication

---
+++ server/utils/auth.js
@@ -0,0 +1,30 @@
+const jwt = require("jsonwebtoken");
+
+const secret = "This is the secret";
+
+module.exports = {
+  authMiddleware: function ({ req }) {
+    let token = req.body.token || req.query.token || req.headers.authorization;
+    if (req.headers.authorization) {
+      token = token.split(" ").pop().trim();
+    }
+
+    if (!token) {
+      return req;
+    }
+
+    try {
+      const { data } = jwt.verify(token, secret);
+      req.user = data;
+    } catch {
+      console.log("Invalid Token");
+    }
+
+    return req;
+  },
+
+  signToken: function ({ email, _id, role }) {
+    const payload = { email, _id, role };
+    return jwt.sign({ data: payload }, secret);
+  },
+};
