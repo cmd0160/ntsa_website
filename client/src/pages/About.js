ea6ab2ded1e8ee4d933fd422886ac9ff5cc16a6a
cdtest <corydavis1989@gmail.com>
2022-06-14 18:48:57 -0500

    about section added to react app

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,11 +1,187 @@
 import React from 'react'
+import ntsaPresident from "../../src/images/christine-cd.jpeg";
 
 
 const About = () => {
 
   return (
-    
-    <div>About</div>
+<div>
+    <section id="about_hero">
+    <div class="container">
+        <h1>About Our Organization</h1>
+    </div>
+    </section>
+
+<section id="main-content">
+    <div class="container">
+
+        <h2 class="p-3">Our Mission</h2>
+        <p>The North Texas Suzuki Association exists to coordinate, support and publicize Suzuki events in North
+            Texas.
+            We promote Suzuki teaching and training among teachers, parents, students and the general public. The
+            highest standards of excellence in teaching and playing are encouraged among our members. NTSA also
+            serves
+            as a vehicle for communication among Suzuki teachers and fosters growth of the Suzuki philosophy in
+            Texas.
+            <br></br>
+            <br></br>
+            The NTSA is a chapter affiliate of the <span><a href="https://suzukiassociation.org/">Suzuki Association
+                    of The Americas</a></span>.
+        </p>
+        <hr></hr>
+    </div>
+</section>
+
+<section id="board-members">
+    <h2 class="p-3 b-members">Board Members</h2>
+    <div class="card-container">
+
+        <div class="card">
+            <div class="image-container py-1">
+                <div class="image-holder">
+                    <img src={ntsaPresident} alt="" />
+                </div>
+                <i class="fa fa-envelope fa-2x"></i>
+                <i class="fab fa-linkedin fa-2x"></i>
+            </div>
+            <div class="info-container py-1">
+                <h3>President</h3>
+                <h4>Christine Collins-Davis</h4>
+                <p>Christine Collins-Davis attended the University of North Texas for her Bachelors Degree in Music
+                    Education and Elementary Music Education. She recently graduated from the University of Texas at
+                    Arlington with a Masters of Music Education. She has been teaching in Hurst-Euless-Bedford ISD
+                    as a Suzuki Strings Specialist since 2016. Christine is passionate about teaching using the
+                    Suzuki ideal of creating better citizens through music lessons.
+                </p>
+            </div>
+        </div>
+
+        <div class="card card-reverse">
+            <div class="info-container py-1">
+                <h3>President</h3>
+                <h4>Christine Collins-Davis</h4>
+                <p>Christine Collins-Davis attended the University of North Texas for her Bachelors Degree in Music
+                    Education and Elementary Music Education. She recently graduated from the University of Texas at
+                    Arlington with a Masters of Music Education. She has been teaching in Hurst-Euless-Bedford ISD
+                    as a Suzuki Strings Specialist since 2016. Christine is passionate about teaching using the
+                    Suzuki ideal of creating better citizens through music lessons.
+                </p>
+            </div>
+            <div class="image-container py-1">
+                <div class="image-holder">
+                    <img src={ntsaPresident} alt="" />
+                </div>
+                <i class="fa fa-envelope fa-2x"></i>
+                <i class="fab fa-linkedin fa-2x"></i>
+            </div>
+
+        </div>
+
+        <div class="card">
+            <div class="image-container py-1">
+                <div class="image-holder">
+                    <img src={ntsaPresident} alt="" />
+                </div>
+                <i class="fa fa-envelope fa-2x"></i>
+                <i class="fab fa-linkedin fa-2x"></i>
+
+            </div>
+            <div class="info-container py-1">
+                <h3>President</h3>
+                <h4>Christine Collins-Davis</h4>
+                <p>Christine Collins-Davis attended the University of North Texas for her Bachelors Degree in Music
+                    Education and Elementary Music Education. She recently graduated from the University of Texas at
+                    Arlington with a Masters of Music Education. She has been teaching in Hurst-Euless-Bedford ISD
+                    as a Suzuki Strings Specialist since 2016. Christine is passionate about teaching using the
+                    Suzuki ideal of creating better citizens through music lessons.
+                </p>
+            </div>
+        </div>
+
+        <div class="card card-reverse">
+            <div class="info-container py-1">
+                <h3>President</h3>
+                <h4>Christine Collins-Davis</h4>
+                <p>Christine Collins-Davis attended the University of North Texas for her Bachelors Degree in Music
+                    Education and Elementary Music Education. She recently graduated from the University of Texas at
+                    Arlington with a Masters of Music Education. She has been teaching in Hurst-Euless-Bedford ISD
+                    as a Suzuki Strings Specialist since 2016. Christine is passionate about teaching using the
+                    Suzuki ideal of creating better citizens through music lessons.
+                </p>
+            </div>
+            <div class="image-container py-1">
+                <div class="image-holder">
+                    <img src={ntsaPresident} alt="" />
+                </div>
+                <i class="fa fa-envelope fa-2x"></i>
+                <i class="fab fa-linkedin fa-2x"></i>
+            </div>
+        </div>
+    </div>
+    <hr></hr>
+</section>
+
+<section id="main-content">
+    <div class="container">
+
+        <h2 class="p-3">Code Of Ethics</h2>
+        <h4 class="p-2">North Texas Suzuki Association Code of Ethics <br></br>
+            Created by the NTSA Executive Committee, 2002</h4>
+        <p class="code-of-ethics">
+
+            This Code of Ethics was adopted by the North Texas Suzuki Association’s Board to standardize
+            rules of conduct for Suzuki teachers in relation to pupils, parents, teachers, and the public. It
+            shall be the obligation of every member to maintain the highest standards of moral and
+            professional conduct and personal integrity, always keeping the best interest of children at heart.
+            <br></br>
+            <br></br>
+            1. I will not recruit students who are currently studying with another teacher. Upon leaving a
+            program or school, I will not solicit students who are enrolled by that program/school
+            without the express consent of the director of the school/program.
+            <br></br>
+            <br></br>
+            2. I will accept a student who wishes to transfer to my studio only after I have notified the prior
+            teacher/school involved or with any teacher with whom the student has terminated lessons
+            with in the past 3 months. I will not accept a student who has outstanding financial
+            obligations to another teacher.
+            <br></br>
+            <br></br>
+            3. I will not make negative comments regarding other teachers/studios in the presence of
+            parents/student.
+            <br></br>
+            <br></br>
+            4. If I have doubts concerning the competence and/or motives of another teacher, I will voice
+            them to the teacher involved, or I will remain silent. I will not make negative comments
+            regarding students/parents in the presence of other students/parents.
+            <br></br>
+            <br></br>
+            5. I will continue to grow professionally through educational opportunities and through active
+            participation in the music community. I will support my fellow colleagues who continue to
+            educate themselves and grow through their contacts in the music community.
+            <br></br>
+            <br></br>
+            6. As a Suzuki teacher, I use review, listening, group activities, weekly reading assignments,
+            and parent involvement, where appropriate, to help develop talent and motivate my students.
+            <br></br>
+            <br></br>
+            7. In advertising, I will adhere to facts reflective of my training, skills, and abilities as a Suzuki
+            teacher. I will advise parents to check with SAA about the training records of teachers.
+            <br></br>
+            <br></br>
+            8. If I present a transfer student in performance/competition who has studied fewer than 6
+            consecutive months with me, I will acknowledge the work of the former teacher.
+            <br></br>
+            <br></br>
+            9. I realize that I have a heightened sense of responsibility when I share a Suzuki student or
+            family with another Suzuki teacher and will observe strict adherence to the Code of Ethics, parent education, and the Suzuki Philosophy.
+            <br></br>
+            <br></br>
+            10. I will abide by the SAA Code of Ethics.
+        </p>
+
+    </div>
+    </section>
+</div>
   )
 }
 
