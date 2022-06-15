ef322cd432c66dd559d5beec0c0be8b1167048b4
cdtest <corydavis1989@gmail.com>
2022-06-15 17:22:02 -0500

    added login card and ashley skidmore information

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -2,7 +2,40 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <section id="login-form">
+    <div className="container">
+
+        <div className="exit-row">
+            <i className="fa-solid fa-xmark"></i>
+        </div>
+
+        <div className="center">
+            <h3>North Texas Suzuki Association</h3>
+        </div>
+
+        <form>
+            <input className="form-input" type="text" placeholder="EMAIL ADDRESS" name="email" id="login-email" />
+            <input className="form-input" type="password" placeholder="PASSWORD" name="password" id="login-password" />
+            <div className="row justify-content-center">
+
+                <div className="sign-up-button center">
+                    <button className=" btn btn-primary" type="submit">
+                        LOGIN
+                    </button>
+                </div>
+            </div>
+        </form>
+
+        <div className="not-a-member center">
+            <p>Not a member yet? Sign up here!</p>
+        </div>
+        <div className="sign-up-button center">
+            <button className=" btn btn-primary" type="submit">
+                SIGNUP
+            </button>
+        </div>
+    </div>
+</section>
   )
 }
 
