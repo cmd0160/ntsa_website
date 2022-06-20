ca79d33b5c0a4eef0248167300e355b147597ac5
cdtest <corydavis1989@gmail.com>
2022-06-20 16:36:05 -0500

    contact form is built

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div className="contact-con">
+        <h1 className="contact-title p-2">Contact Us</h1>
+        <form className="">
+          <input
+            type="text"
+            className="text name"
+            placeholder="Name"
+            name="name"
+          />
+          <input
+            type="text"
+            className="text email"
+            placeholder="Email Address"
+            name="email"
+          />
+          <input
+            type="text"
+            className="text subject"
+            placeholder="Subject"
+            name="subject"
+          />
+          <textarea
+            type="text"
+            className="text message"
+            placeholder="Message"
+            name="message"
+          />
+          <div>
+            <button className="btn btn-primary">Submit</button>
+          </div>
+        </form>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
