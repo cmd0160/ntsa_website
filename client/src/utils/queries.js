e4bd0437124710ab84cad30df68997d6d751ba98
cdtest <corydavis1989@gmail.com>
2022-06-21 21:28:50 -0500

    added queries and fixed auth.js

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,46 +1,40 @@
-import decode from "jwt-decode";
+import { gql } from "@apollo/client";
 
-class AuthService {
-  getProfile() {
-    return decode(this.getToken());
-  }
-
-  loggedIn() {
-    // Checks if there is a saved token and it's still valid
-    const token = this.getToken();
-    return !!token && !this.isTokenExpired(token); // handwaiving here
-  }
-
-  isTokenExpired(token) {
-    try {
-      const decoded = decode(token);
-      if (decoded.exp < Date.now() / 1000) {
-        return true;
-      } else {return false;}
-     
-    } catch (err) {
-      return false;
+export const QUERY_CURRENT_USER = gql`
+  query Me {
+    me {
+      _id
+      firstName
+      lastName
+      email
+      role
+      membershipDate
     }
   }
-  getToken() {
-    // Retrieves the user token from localStorage
-    return localStorage.getItem("id_token");
+`;
+
+export const QUERY_ALL_USERS = gql`
+  query Users {
+    users {
+      _id
+      email
+      firstName
+      lastName
+      role
+      membershipDate
+    }
   }
+`;
 
-  login(idToken) {
-    // Saves user token to localStorage
-    localStorage.setItem("id_token", idToken);
-
-    window.location.assign("/");
+export const QUERY_USER = gql`
+  query User($email: String!) {
+    user(email: $email) {
+      _id
+      firstName
+      lastName
+      email
+      role
+      membershipDate
+    }
   }
-
-  logout() {
-    // Clear user token and profile data from localStorage
-    // axios.defaults.headers.common["Authorization"] = null;
-    localStorage.removeItem("id_token");
-    // this will reload the page and reset the state of the application
-    window.location.assign("/");
-  }
-}
-
-export default new AuthService();
+`;
