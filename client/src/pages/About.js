4ce54ec69acf3e80470c27624cd3a2d94df14a27
CD MacBook <corydavis1989@gmail.com>
2022-06-23 12:07:25 -0500

    added new board member information

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ntsaPresident from "../../src/images/christine-cd.jpeg";
 import ntsaTreasurer from "../../src/images/ashliePic.jpeg";
+import vicePresident from "../../src/images/vicePresident.jpg";
+import secretary from "../../src/images/secretary.JPG"
 
 const About = () => {
   return (
@@ -65,77 +67,68 @@ const About = () => {
 
           <div className="card card-reverse">
             <div className="info-container py-1">
-              <h3>President</h3>
-              <h4>Christine Collins-Davis</h4>
+              <h3>Vice President</h3>
+              <h4>Sarah Gonzalez</h4>
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Consequuntur, blanditiis? Impedit aliquid qui modi. Consequuntur
-                porro repudiandae enim nesciunt facilis soluta? Ut tempore modi
-                eum beatae, doloribus fugit quod eaque explicabo praesentium
-                repellendus eius quo. Fugiat molestias perferendis sapiente at
-                eveniet aut facilis saepe ducimus unde quia delectus natus
-                praesentium libero illum molestiae, consequuntur vitae ipsum
-                explicabo repellat doloremque nesciunt blanditiis cum? Adipisci
-                facere impedit, eaque sunt laudantium ratione eum quaerat
-                inventore. Rem quae quas voluptates nulla voluptas consectetur
-                dicta modi, beatae mollitia reiciendis esse! Natus accusantium
-                nam soluta cumque distinctio, temporibus totam aspernatur in?
-                Error aperiam optio quidem? Quod quos voluptatem sint
-                consequuntur praesentium cumque ipsam quis soluta, omnis ut
-                itaque aliquid sapiente provident hic inventore ratione earum
-                obcaecati deleniti tempora quisquam. Hic error corporis
-                cupiditate, deserunt dolorem sed architecto dolores. Earum
-                quibusdam nesciunt dolorem. Quidem fuga velit, numquam ipsum
-                laboriosam quo impedit repellat nesciunt hic fugiat labore
-                praesentium recusandae! Corrupti quasi sed nam laudantium,
-                architecto ipsa repudiandae natus quisquam cumque, aut facilis
-                vitae deleniti rerum repellat eos aspernatur esse labore odio
-                voluptates. Quisquam sapiente consequatur odit iusto reiciendis,
-                fugiat eius quod temporibus! Magni laborum laboriosam sit
-                consequuntur aperiam ipsa provident perspiciatis, corporis
-                quidem fugit tempore, quibusdam quisquam vitae molestias
-                repudiandae maiores officia alias sapiente! Quaerat laborum quas
-                ut praesentium eius deserunt illo distinctio esse quidem!
-                Maiores saepe porro at officia dolorum eveniet. Saepe veritatis
-                dicta, nostrum magni, reiciendis quaerat nulla sint facere
-                expedita suscipit eius recusandae exercitationem fugiat porro
-                consectetur molestiae libero, reprehenderit modi deserunt
-                voluptates nobis maxime eligendi quae. Sunt minus, obcaecati
-                asperiores, at incidunt rem pariatur aut odio facilis impedit et
-                earum molestiae officia alias fuga assumenda nostrum cum iusto.
-                Ducimus, in fuga laboriosam magnam facere magni sapiente unde
-                facilis nulla nam eum, ullam soluta. Quam corrupti rerum fuga
-                explicabo. Aspernatur, quaerat? Praesentium enim harum a. Illum
-                cupiditate incidunt magni possimus culpa dicta? Maxime a
-                incidunt nulla deserunt! Iste veniam unde tempore cumque
-                exercitationem eligendi optio repudiandae nihil dolorem,
-                expedita deserunt nobis autem obcaecati harum laborum eius
-                placeat ullam nesciunt, quam iure voluptas. Quae fuga a vel
-                impedit nemo animi sequi voluptatibus earum corrupti
-                reprehenderit, corporis quibusdam error cum non cumque rerum
-                consequuntur ut consequatur totam adipisci assumenda laboriosam,
-                molestias delectus itaque! Porro dolores eius modi natus
-                voluptatibus veritatis fuga beatae assumenda totam animi maxime
-                voluptatum aperiam asperiores, vero atque aliquam ab ipsa autem.
-                Doloremque deserunt doloribus tempore facilis vitae ea culpa
-                ratione quasi fugiat quibusdam ipsum officiis impedit recusandae
-                sapiente totam consequuntur dolorum vel nesciunt laborum,
-                accusantium corporis quam animi, dolorem eaque. Alias autem
-                debitis culpa aut deleniti nostrum? Voluptates qui odit maxime
-                esse pariatur ipsa nemo autem at aperiam magni. Nihil, nulla id
-                suscipit accusamus vero molestiae architecto inventore iste quo
-                quia cupiditate assumenda exercitationem quod dolorum fuga ipsum
-                ratione asperiores magnam tempore sed molestias similique!
-                Inventore ducimus ipsa unde consequatur perferendis sequi
-                incidunt eligendi, ex, doloribus voluptatibus esse voluptate,
-                rem ratione neque! Ut, molestiae! Dolorem, accusamus aliquid.
-                Amet a eos modi, reiciendis quis iure mollitia consequatur, enim
-                repudiandae aspernatur doloribus. Reiciendis, eaque harum!
+                Sarah Gonzalez began her musical studies on the violin and piano
+                but soon fell in love with the sound of the cello. She was
+                fortunate to study with cellist Luis García-Renart, a student of
+                Pablo Casals, Mstislav Rostropovich, and Aram Khachaturian. Mrs.
+                Gonzalez also received voice training and was very active in her
+                school choral program. She earned a Bachelor of Music Education
+                from Wheaton College’s Conservatory of Music. While at Wheaton,
+                she studied cello with Tanya Lesinsky Carey, past president of
+                the Suzuki Association of the Americas, board member of the
+                International Suzuki Association, and chaired the committee for
+                the Suzuki Cello Book Revisions. Mrs. Gonzalez continued to
+                receive long-term Suzuki training from Dr. Carey and uses her
+                Cello Playing is Easy curriculum in her studio.
+                <br></br>
+                <br></br>
+                Mrs. Gonzalez taught private lessons in cello, violin, viola,
+                double bass, piano, voice, and early childhood music
+                (Musikgarten) in conservatories in Chicago and Miami before
+                moving to Texas. She also served as a music instructor in public
+                and private schools from preschool to high school. She conducted
+                orchestra, choir, show choir, and worship arts.
+                <br></br>
+                <br></br>
+                Mrs. Gonzalez has had the privilege of performing cello in many
+                genres. She participated in the World Cello Congress III, where
+                she had the opportunity to play side-by-side with renowned
+                cellists such as Yo-Yo Ma, Tsuyoshi Tsutsumi, Bernard
+                Greenhouse, among many others. She has played cello for
+                Christian artists, played in a Latin-style band, and substituted
+                for the Illinois Philharmonic Orchestra. Mrs. Gonzalez and her
+                husband also regularly perform for church services, conventions,
+                weddings, and funerals.
+                <br></br>
+                <br></br>
+                Sarah Gonzalez has a full Suzuki cello studio out of her home,
+                including teaching all five of her children. She is also on the
+                faculty of the Fort Worth Youth Orchestra Suzuki program. She
+                directs the orchestra, string ensemble, and brass ensemble at
+                her church and is active in the Suzuki community. She serves on
+                the board of the North Texas Suzuki Association and gives
+                masterclasses to the cello students of the Hurst-Euless-Bedford
+                ISD Suzuki Strings program.
+                <br></br>
+                <br></br>
+                Sarah Gonzalez has a passion for creating in all students a
+                life-long love of music and guiding them to use their talents to
+                serve. Creative in her approach to teaching Mrs. Gonzalez
+                patiently guides students to reach their goals. She teaches
+                music as a language based on her training in the Suzuki Method
+                and linguistics. Her teaching centers on Dr. Suzuki’s philosophy
+                that “every child can.” She firmly believes that every child can
+                learn not only how to play an instrument but how to become a
+                beautiful musician and person. Her hope is that all students can
+                have access to Suzuki music education.
               </p>
             </div>
             <div className="image-container py-1 center">
               <div className="image-holder">
-                <img src={ntsaPresident} alt="" />
+                <img src={vicePresident} alt="" />
               </div>
               <i className="fa fa-envelope fa-2x"></i>
               <i className="fab fa-linkedin fa-2x"></i>
@@ -154,6 +147,29 @@ const About = () => {
               <h3>Treasurer</h3>
               <h4>Ashley Skidmore</h4>
               <p>
+                Ashlie Skidmore is a third-generation Suzuki teacher and Suzuki
+                parent. She began her violin studies at the age of three at the
+                Suzuki Institute of Dallas under Dr. Evelyn Hermann. At the age
+                of four, she was privileged to travel with a Suzuki Institute
+                tour group to Japan to study with Dr. Suzuki. The Dallas tour
+                group, along with the Japanese Suzuki students, also traveled to
+                China to introduce the Suzuki method.
+                <br></br>
+                <br></br>
+                Ashlie was a ten-year member of the Greater Dallas Youth
+                Orchestra Association. During this time, she studied with Irene
+                Mitchell followed by Dr. Ronald Neal at Southern Methodist
+                University during her high school years. She received her
+                Bachelor of Music Degree in Music Education at the University of
+                Texas at Austin. While there, she had the privilege of studying
+                violin with Dr. Eugene Gratovich and music education with Dr.
+                Robert Duke. Following graduation, she taught middle school
+                orchestra in the Austin, Texas area and served as a TMEA Region
+                orchestra coordinator. She has completed various books of Suzuki
+                teacher training with Marilyn O’Boyle, Judy Bossuat-Gallic, and
+                Rolando Freitag.
+                <br></br>
+                <br></br>
                 Ashlie plays both professionally and in community groups
                 throughout the Dallas Fort Worth area. She has been teaching for
                 over 20 years, and enjoys teaching students of all ages and
@@ -171,22 +187,34 @@ const About = () => {
 
           <div className="card card-reverse">
             <div className="info-container py-1">
-              <h3>President</h3>
-              <h4>Christine Collins-Davis</h4>
+              <h3>Secretary</h3>
+              <h4>Amber Gutermuth</h4>
               <p>
-                Christine Collins-Davis attended the University of North Texas
-                for her Bachelors Degree in Music Education and Elementary Music
-                Education. She recently graduated from the University of Texas
-                at Arlington with a Masters of Music Education. She has been
-                teaching in Hurst-Euless-Bedford ISD as a Suzuki Strings
-                Specialist since 2016. Christine is passionate about teaching
-                using the Suzuki ideal of creating better citizens through music
-                lessons.
+                Amber Gutermuth (known as Ms. G by her students) is a violinist
+                and violist who has a heartfelt desire to instill her own love
+                of music in our future generations. She graduated with her
+                Bachelor’s Degree in Music Education in December of 2021 from
+                the University of Texas at Arlington where she studied with Dr.
+                Martha Walvoord, Professor Elizabeth Elsner, and Professor
+                Catherine Forbes. Because she grew up in the method herself
+                starting at the age of 5, she is rather passionate about
+                Suzuki’s pedagogy and all that it provides for students,
+                including the development of exceptional musicianship and
+                instrumental skill, but also characteristics that exemplify what
+                it means to be good citizens of the world. In addition to the
+                Suzuki method, Ms. G frequently enjoys teaching repertoire that
+                exemplifies the different cultures of the world so that we can
+                all learn together how music truly is a universal language! Ms.
+                G does a lot musically outside of her teaching hours including
+                playing in multiple string quartets and a local world music
+                band. Currently Ms. G teaches part time at the Fort Worth Youth
+                Orchestra Suzuki program and full time as an orchestra director
+                at the 5th and 6th grade level in Mansfield ISD.
               </p>
             </div>
             <div className="image-container py-1 center">
               <div className="image-holder">
-                <img src={ntsaPresident} alt="" />
+                <img src={secretary} alt="" />
               </div>
               <i className="fa fa-envelope fa-2x"></i>
               <i className="fab fa-linkedin fa-2x"></i>
