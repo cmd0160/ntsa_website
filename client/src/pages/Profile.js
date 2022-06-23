99a80dec2d941e11724012797da94f71f797780b
CD MacBook <corydavis1989@gmail.com>
2022-06-23 11:15:43 -0500

    added some initial profile page styling

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -4,12 +4,10 @@ import { useQuery } from "@apollo/client";
 import { QUERY_CURRENT_USER } from "../utils/queries";
 
 const Profile = () => {
-
-const {loading, error, data} = useQuery(QUERY_CURRENT_USER);
-if (loading) return 'Loading...';
-if (error) return `Error! ${error.message}`;
-const { firstName, lastName, email } = data.me;
-
+  const { loading, error, data } = useQuery(QUERY_CURRENT_USER);
+  if (loading) return "Loading...";
+  if (error) return `Error! ${error.message}`;
+  const { firstName, lastName, email } = data.me;
 
   return (
     <div>
@@ -17,18 +15,28 @@ const { firstName, lastName, email } = data.me;
         <div className="container">
           <div className="image-container p-1">
             <div className="circular--landscape">
-                {/* <img src={pracPic} alt="" /> */}
+              <img src={pracPic} alt="" />
             </div>
-            <h3 className="p-1">{firstName} {lastName}</h3>
+            <h3 className="p-1">
+              {firstName} {lastName}
+            </h3>
             <h4>Member Status:</h4>
-            <p className="inactive">Inactive</p>
+            <p className="active">Active</p>
           </div>
           <div className="info-container p-1">
             <h3 className="p-2 center">Member Information</h3>
-            <p className="center">First Name: <span>{firstName}</span></p>
-            <p className="center">Last Name: <span>{lastName}</span></p>
-            <p className="center">Email: <span>{email}</span></p>
-            <p className="center" style={{color: "blue"}}>Change Password</p>
+            <div className="p-container">
+              <p>
+                First Name: <span>{firstName}</span>
+              </p>
+              <p>
+                Last Name: <span>{lastName}</span>
+              </p>
+              <p>
+                Email: <span>{email}</span>
+              </p>
+              <p style={{ color: "blue" }}>Change Password</p>
+            </div>
           </div>
         </div>
       </section>
