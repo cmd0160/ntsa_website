64d3f6e5f2bd891fda9a406f0d4c0beb511b9757
cdtest <corydavis1989@gmail.com>
2022-07-10 14:04:43 -0500

    DEPLOYING

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-    process.env.MONGODB_URI || "mongodb://localhost:27017/ntsa_website",
+    process.env.MONGODB_URI || "mongodb://localhost:27017/NTSA_website",
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
