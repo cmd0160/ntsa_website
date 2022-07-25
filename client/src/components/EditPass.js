20a6e17f5e5746d467b787701c092a63b5f668e0
cdtest <corydavis1989@gmail.com>
2022-07-25 07:51:08 -0500

    working on password functionality

--- client/src/components/EditPass.js
+++ client/src/components/EditPass.js
@@ -1,7 +1,45 @@
-import React from 'react'
+import { useState } from 'react'
+import { useMutation } from '@apollo/client';
+import { UPDATE_USER } from "../utils/mutations";
 
 const EditPass = (props) => {
-  const { _id } = props.user;
+
+  const [formState, setFormState] = useState({
+    password: "",
+  });
+
+  const [updateUser, { error }] = useMutation(UPDATE_USER);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target.value;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  const newInfo = async (event) => {
+    const { _id } = props.user;
+
+    event.preventDefault();
+    const dataObject = {
+        password: event.target[0].value
+    }
+    console.log(dataObject);
+    try {
+      const { data } = await updateUser({
+        variables: {
+          id: _id,
+          ...dataObject,
+        },
+      });
+      console.log(data);
+      alert('Password Updated!')
+    } catch {
+      console.log(error);
+    }
+  };
 
   
   return (
@@ -9,21 +47,22 @@ const EditPass = (props) => {
     <h5 className="p-h center">Update Password:</h5>
     <div className="m-info">
       <div id="edit-form">
-        <form>
+        <form onSubmit={newInfo}>
         <input
             className="signup-input"
             type="password"
             placeholder="New Password*"
             name="password"
-            id="login-password"
+            id="password"
+            onChange={handleChange}
           />
-          <input
+          {/* <input
             className="signup-input"
             type="password"
             placeholder="Confirm New Password*"
-            name="password"
-            id="login-password"
-          />
+            name="confirmPassword"
+            id="confirmPassword"
+          /> */}
           <div className="sign-up-button center ">
             <button className=" btn btn-primary" type="submit">
               SUBMIT
