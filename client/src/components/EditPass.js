d7b64e184714cf53886f7a0210362f22258d6992
cdtest <corydavis1989@gmail.com>
2022-07-30 21:11:43 -0500

    password is working on FE

--- client/src/components/EditPass.js
+++ client/src/components/EditPass.js
@@ -1,9 +1,8 @@
-import { useState } from 'react'
-import { useMutation } from '@apollo/client';
+import { useState } from "react";
+import { useMutation } from "@apollo/client";
 import { UPDATE_USER } from "../utils/mutations";
 
 const EditPass = (props) => {
-
   const [formState, setFormState] = useState({
     password: "",
   });
@@ -12,7 +11,7 @@ const EditPass = (props) => {
 
   const handleChange = (event) => {
     const { name, value } = event.target.value;
-
+    
     setFormState({
       ...formState,
       [name]: value,
@@ -21,59 +20,60 @@ const EditPass = (props) => {
 
   const newInfo = async (event) => {
     const { _id } = props.user;
-
+      console.log(_id)
     event.preventDefault();
     const dataObject = {
-        password: event.target[0].value
-    }
+      password: event.target[0].value,
+    };
     console.log(dataObject);
     try {
       const { data } = await updateUser({
         variables: {
-          id: _id,
+          _id: _id,
           ...dataObject,
         },
       });
       console.log(data);
-      alert('Password Updated!')
+      alert("Password Updated!");
     } catch {
       console.log(error);
     }
   };
 
-  
   return (
     <div id="editPass-section">
-    <h5 className="p-h center">Update Password:</h5>
-    <div className="m-info">
-      <div id="edit-form">
-        <form onSubmit={newInfo}>
-        <input
-            className="signup-input"
-            type="password"
-            placeholder="New Password*"
-            name="password"
-            id="password"
-            onChange={handleChange}
-          />
-          {/* <input
+      <h5 className="p-h center">Update Password:</h5>
+      <div className="m-info">
+        <div id="edit-form">
+          <form onSubmit={newInfo}>
+            <input
+              className="signup-input"
+              type="password"
+              placeholder="New Password*"
+              name="password"
+              id="password"
+              onBlur={handleChange}
+            />
+            {/* <input
             className="signup-input"
             type="password"
             placeholder="Confirm New Password*"
             name="confirmPassword"
             id="confirmPassword"
           /> */}
-          <div className="sign-up-button center ">
-            <button className=" btn btn-primary" type="submit">
-              SUBMIT
-            </button>
-              <p style={{ color: "blue" }} onClick={props.togglePass} >Back To Member Information</p>
-          </div>
-        </form>
+            <div className="sign-up-button center ">
+              <button className=" btn btn-primary" type="submit">
+                SUBMIT
+              </button>
+              <p style={{ color: "blue" }} onClick={props.togglePass}>
+                Back To Member Information
+              </p>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default EditPass
+export default EditPass;
